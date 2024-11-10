43481d1dbd9ca13bf88a843007d1e1e057d905e2
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-11-10 23:30:11 +0300

    Added custom annotation view.

--- iLocation/Scenes/Main/MainViewController.swift
+++ iLocation/Scenes/Main/MainViewController.swift
@@ -4,13 +4,16 @@ import MapKit
 final class MainViewController: UIViewController {
     private lazy var mapView = MKMapView(frame: view.bounds)
 
+    //  MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         setupHierarchy()
         configureMapRegion()
         setupMapAnnotation()
     }
 
+    //  MARK: - Private
     private func setupHierarchy() {
         view.addSubview(mapView)
         NSLayoutConstraint.activate([
@@ -32,18 +35,30 @@ final class MainViewController: UIViewController {
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: 37.334774, longitude: -122.008992)
         annotation.title = "Apple"
-        annotation.subtitle = "HeadQuarter"
+        annotation.subtitle = "Cupertino, CA"
         
         let salesForceAnnotation = MKPointAnnotation()
         salesForceAnnotation.coordinate = CLLocationCoordinate2D(latitude: 37.79055, longitude: -122.38916)
         salesForceAnnotation.title = "Salesforce"
-        salesForceAnnotation.subtitle = "Company"
+        salesForceAnnotation.subtitle = "Financial District, SF"
     
         mapView.addAnnotations([annotation, salesForceAnnotation])
         mapView.showAnnotations(mapView.annotations, animated: true)
     }
 }
 
+//  MARK: - MKMapViewDelegate
+extension MainViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+        let customAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation_identifier")
+        customAnnotationView.canShowCallout = true
+        customAnnotationView.image = UIImage(named: "map")
+        
+        return customAnnotationView
+    }
+}
+
+//  MARK: - SWIFTUI Preview
 import SwiftUI
 
 struct MainPreview: PreviewProvider {

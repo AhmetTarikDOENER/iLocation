49e09471dd4db9c3c2fa53cbb5d8ecc36e7bfc9b
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-11-10 16:02:56 +0300

    Added SwiftUI preview support to be able to build faster, and setted region manually for now.

--- iLocation/Scenes/Main/MainViewController.swift
+++ iLocation/Scenes/Main/MainViewController.swift
@@ -2,9 +2,15 @@ import UIKit
 import MapKit
 
 final class MainViewController: UIViewController {
+    private lazy var mapView = MKMapView(frame: view.bounds)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let mapView = MKMapView(frame: view.frame)
+        setupHierarchy()
+        configureMapRegion()
+    }
+
+    private func setupHierarchy() {
         view.addSubview(mapView)
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -12,5 +18,31 @@ final class MainViewController: UIViewController {
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    private func configureMapRegion() {
+        let centeredCoordinates = CLLocationCoordinate2D(latitude: 37.334774, longitude: -122.008992)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.01)
+        let region =  MKCoordinateRegion(center: centeredCoordinates, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+            .ignoresSafeArea()
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        typealias UIViewControllerType = MainViewController
+        
+        func makeUIViewController(context: Context) -> MainViewController { 
+            MainViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainViewController, context: Context) { }
     }
 }

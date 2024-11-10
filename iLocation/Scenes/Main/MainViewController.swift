372998a216dc6093ee7b21f5d2b477793826738c
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-11-10 15:05:45 +0300

    Started to organize the foldering structure.

---
+++ iLocation/Scenes/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import MapKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let mapView = MKMapView(frame: view.frame)
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}

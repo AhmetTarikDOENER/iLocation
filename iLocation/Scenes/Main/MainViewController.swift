9f220257883dad1a0d19003a74bbbe35be04b355
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-11-12 21:49:03 +0300

    Fixed bug related to relocate the map when searching and decided to go with Combine framework respond text changes.

--- iLocation/Scenes/Main/MainViewController.swift
+++ iLocation/Scenes/Main/MainViewController.swift
@@ -1,18 +1,27 @@
 import UIKit
 import MapKit
+import Combine
 
 final class MainViewController: UIViewController {
-    private lazy var mapView = MKMapView(frame: view.bounds)
-    private let textField = UITextField()
+    private lazy var mapView = MKMapView(frame: view.frame)
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Search"
+        textField.leftView = .init(frame: .init(x: 0, y: 0, width: 10, height: 10))
+        textField.leftViewMode = .always
+        
+        return textField
+    }()
 
     //  MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        setupHierarchy()
         configureMapRegion()
-        performLocalSearch()
+        setupHierarchy()
         setupSearchUI()
+        performLocalSearch()
     }
 
     //  MARK: - Private
@@ -28,19 +37,23 @@ final class MainViewController: UIViewController {
     
     private func configureMapRegion() {
         let centeredCoordinates = CLLocationCoordinate2D(latitude: 37.334774, longitude: -122.008992)
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region =  MKCoordinateRegion(center: centeredCoordinates, span: span)
+        
         mapView.setRegion(region, animated: true)
     }
     
     private func performLocalSearch() {
+        configureMapRegion()
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = textField.text
+        request.naturalLanguageQuery = searchTextField.text
         request.region = mapView.region
+        request.pointOfInterestFilter = .includingAll
+        
         let localSearch = MKLocalSearch(request: request)
         localSearch.start { response, error in
             if let error = error {
-                print("Failed local search")
+                print("Failed local search", error)
                 return
             }
             self.mapView.removeAnnotations(self.mapView.annotations)
@@ -55,25 +68,19 @@ final class MainViewController: UIViewController {
     }
 
     private func setupSearchUI() {
-        let textField = buildTextField()
         let containerView = buildContainerView()
         view.addSubview(containerView)
-        containerView.addSubview(textField)
+        containerView.addSubview(searchTextField)
         NSLayoutConstraint.activate([
             containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             containerView.heightAnchor.constraint(equalToConstant: 50),
-            textField.topAnchor.constraint(equalTo: containerView.topAnchor),
-            textField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            textField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            textField.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            searchTextField.topAnchor.constraint(equalTo: containerView.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            searchTextField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            searchTextField.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
         ])
-        textField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
-    }
-    
-    @objc private func handleSearchChanges() {
-        performLocalSearch()
     }
 }
 //  MARK: - MainViewController+UIComponentBuilders
@@ -87,15 +94,6 @@ extension MainViewController {
         containerView.backgroundColor = .systemBackground
         
         return containerView
-    }
-    
-    fileprivate func buildTextField() -> UITextField {
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "Search"
-        textField.leftView = .init(frame: .init(x: 0, y: 0, width: 10, height: 10))
-        textField.leftViewMode = .always
-        
-        return textField
     }
 }
 
@@ -117,7 +115,7 @@ extension MKMapItem {
 //  MARK: - MKMapViewDelegate
 extension MainViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
-        let customAnnotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "annotation_identifier")
+        let customAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation_identifier")
         customAnnotationView.canShowCallout = true
         customAnnotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
 

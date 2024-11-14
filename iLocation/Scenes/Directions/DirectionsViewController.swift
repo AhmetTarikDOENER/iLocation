833749e1341b98026496f6aebb98c50a3fff351a
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-11-14 23:08:22 +0300

    Added a function to indicate starting and endpoint annotations on the mapView manually for now.

--- iLocation/Scenes/Directions/DirectionsViewController.swift
+++ iLocation/Scenes/Directions/DirectionsViewController.swift
@@ -4,48 +4,56 @@ import SwiftUI
 
 final class DirectionsViewController: UIViewController {
     private let mapView = MKMapView()
+    private lazy var navigationBarView = UIView()
     
     //  MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHierarchy()
-        setupNavigationBar()
         configureMapRegion()
         mapView.showsUserLocation = true
+        configureFromToAnnotations()
     }
     
     //  MARK: - Private
     private func setupHierarchy() {
+        view.addSubview(navigationBarView)
         view.addSubview(mapView)
         mapView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            mapView.topAnchor.constraint(equalTo: view.topAnchor),
-            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
-    private func configureMapRegion() {
-        let centeredCoordinates = CLLocationCoordinate2D(latitude: 37.334774, longitude: -122.008992)
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region =  MKCoordinateRegion(center: centeredCoordinates, span: span)
-        
-        mapView.setRegion(region, animated: true)
-    }
-    
-    private func setupNavigationBar() {
-        let navigationBarView = UIView()
         navigationBarView.translatesAutoresizingMaskIntoConstraints = false
         navigationBarView.backgroundColor = #colorLiteral(red: 0.1215686275, green: 0.5607843137, blue: 0.9647058824, alpha: 1)
         navigationBarView.setupShadow(opacity: 1, radius: 5)
         view.addSubview(navigationBarView)
         NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             navigationBarView.topAnchor.constraint(equalTo: view.topAnchor),
             navigationBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             navigationBarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100)
         ])
+    }
+    
+    private func configureMapRegion() {
+        let centeredCoordinates = CLLocationCoordinate2D(latitude: 37.334774, longitude: -122.008992)
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        let region =  MKCoordinateRegion(center: centeredCoordinates, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    private func configureFromToAnnotations() {
+        let startingAnnotation = MKPointAnnotation()
+        startingAnnotation.coordinate = .init(latitude: 37.78807, longitude: -122.50111)
+        startingAnnotation.title = "Start"
+        
+        let endingAnnotation = MKPointAnnotation()
+        endingAnnotation.coordinate = .init(latitude: 37.331352, longitude: -122.030331)
+        endingAnnotation.title = "End"
+        mapView.addAnnotations([startingAnnotation, endingAnnotation])
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
 }
 
@@ -54,6 +62,7 @@ struct DirectionsPreview: PreviewProvider {
     static var previews: some View {
         ContainerView()
             .ignoresSafeArea(.all)
+//            .environment(\.colorScheme, .dark)
     }
     
     struct ContainerView: UIViewControllerRepresentable {
